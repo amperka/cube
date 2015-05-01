524f520c8dd6bc0ff513a1046321eb32193b541b
Victor Nakoryakov <nail.xx@gmail.com>
2015-05-01 14:41:02 +0300

    add: Mode UI

--- main.py
+++ main.py
@@ -19,8 +19,99 @@ class CubeApp(wx.App):
         frame.Show()
         self.MainLoop()
 
+#==============================================================================
+class ActionPanel(wx.Panel):
+    def __init__(self, parent, device):
+        super(ActionPanel, self).__init__(parent)
+        self.device = device
+        self.panels = {}
+        self.active_panel = None
+        self.InitUI()
+
+    def InitUI(self):
+        self.mode_combobox = wx.ComboBox(self, style=wx.CB_READONLY)
+        self.mode_combobox.Bind(wx.EVT_COMBOBOX, self.OnComboboxChanged)
+
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.mode_combobox, 0, wx.EXPAND | wx.ALL, 10)
+        self.AddMode(u'Вручную', ManualControlPanel(self, self.device))
+        self.AddMode(u'GMail', ImapPanel(self, self.device, False))
+        self.AddMode(u'Mail.ru', ImapPanel(self, self.device, False))
+        self.AddMode(u'Произвольный IMAP', ImapPanel(self, self.device, True))
+        self.ShowPanel(u'Вручную')
+        self.SetSizer(self.sizer)
+
+    def AddMode(self, name, panel):
+        self.panels[name] = panel
+        self.mode_combobox.Append(name)
+        panel.Hide()
+        self.sizer.Add(panel, 1, wx.EXPAND | wx.ALL, 10)
+
+    def ShowPanel(self, name):
+        if self.active_panel:
+            self.active_panel.Hide()
+        self.panels[name].Show()
+        self.active_panel = self.panels[name]
+        self.mode_combobox.SetSelection(self.mode_combobox.FindString(name))
+        self.Layout()
+
+    def OnComboboxChanged(self, event):
+        self.ShowPanel(self.mode_combobox.GetValue())
 
 
+#==============================================================================
+class ImapPanel(wx.Panel):
+    def __init__(self, parent, device, show_imap_host_port):
+        super(ImapPanel, self).__init__(parent)
+        self.device = device
+        self.show_imap_host_port = show_imap_host_port
+        self.InitUI()
+
+    def InitUI(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        credentials_panel = self.CreateCredentialsUI()
+        run_button = wx.Button(self, label=u'Поехали!')
+
+        sizer.Add(credentials_panel, 1, wx.EXPAND)
+        sizer.Add(run_button, 0, wx.EXPAND | wx.ALL, 10)
+        self.SetSizer(sizer)
+
+    def CreateCredentialsUI(self):
+        cp = wx.Panel(self)
+
+        sizer = wx.FlexGridSizer(rows=0, cols=2, vgap=10, hgap=10)
+        sizer.AddGrowableCol(1, 1)
+
+        if self.show_imap_host_port:
+            self.host_input = wx.TextCtrl(cp)
+            self.port_input = wx.TextCtrl(cp)
+            sizer.AddMany([
+                wx.StaticText(cp, label=u"Хост"),
+                (self.host_input, 1, wx.EXPAND),
+
+                wx.StaticText(cp, label=u"Порт"),
+                (self.port_input, 1, wx.EXPAND),
+            ])
+
+        self.login_input = wx.TextCtrl(cp)
+        self.password_input = wx.TextCtrl(cp)
+        sizer.AddMany([
+            wx.StaticText(cp, label=u"Логин"),
+            (self.login_input, 1, wx.EXPAND),
+
+            wx.StaticText(cp, label=u"Пароль"),
+            (self.password_input, 1, wx.EXPAND),
+        ])
+
+        cp.SetSizer(sizer)
+
+        return cp
+
+
+
+
+#==============================================================================
 class ManualControlPanel(wx.Panel):
     def __init__(self, parent, device):
         super(ManualControlPanel, self).__init__(parent)
@@ -81,9 +172,9 @@ class PortSelectionPanel(wx.Panel):
         ok_button = wx.Button(self, label=u'OK')
         ok_button.Bind(wx.EVT_BUTTON, self.OnOK)
 
-        box.Add(wx.StaticText(self, label=u'К какому порту подключен куб?'), 1, wx.EXPAND | wx.ALL, 10)
+        box.Add(wx.StaticText(self, label=u'К какому порту подключен куб?'), 0, wx.EXPAND | wx.ALL, 10)
         box.Add(self.listbox, 1, wx.EXPAND | wx.ALL, 10)
-        box.Add(ok_button, 1, wx.EXPAND | wx.ALL, 10)
+        box.Add(ok_button, 0, wx.EXPAND | wx.ALL, 10)
 
         self.SetSizer(box)
 
@@ -100,7 +191,7 @@ class PortSelectionPanel(wx.Panel):
 class MainFrame(wx.Frame):
     def __init__(self, parent, device):
         super(MainFrame, self).__init__(
-            parent, title=u'Технокуб', size=(320, 240))
+            parent, title=u'Технокуб', size=(320, 360))
             
         self.device = device
         self.panels = {}
@@ -116,7 +207,7 @@ class MainFrame(wx.Frame):
 
         self.AddPanel('discovery', DiscoveryPanel(self))
         self.AddPanel('port_selection', PortSelectionPanel(self, self.device))
-        self.AddPanel('manual_control', ManualControlPanel(self, self.device))
+        self.AddPanel('action', ActionPanel(self, self.device))
         self.ShowPanel('discovery')
 
         self.SetSizer(self.sizer)
@@ -148,17 +239,16 @@ class MainFrame(wx.Frame):
             self.panels['port_selection'].Bind(EVT_PORT_SELECTED, self.OnPortSelected)
             self.ShowPanel('port_selection')
         elif len(ports) == 1:
-            self.Connect(ports[0])
-            self.ShowPanel('manual_control')
+            self.ConnectDevice(ports[0])
         else:
             raise NotImlementedError
 
     def OnPortSelected(self, event):
-        self.Connect(event.port)
+        self.ConnectDevice(event.port)
 
-    def Connect(self, port):
+    def ConnectDevice(self, port):
         self.device.connect(port)
-        self.ShowPanel('manual_control')
+        self.ShowPanel('action')
 
 
 
